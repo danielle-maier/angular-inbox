2fae3ed84eddc1cd97643e798740daca746ee26f
Danielle Maier <danielle.e.maier@gmail.com>
2017-09-06 14:40:24 -0600

    fixes 👏

--- app/inbox/inbox_controller.js
+++ app/inbox/inbox_controller.js
@@ -4,11 +4,16 @@
     .module('app')
     .controller('inboxController', inboxController)
 
-  function inboxController() {
+  function inboxController($http) {
     const vm = this;
     vm.$onInit = function() {
-      var data = angular.fromJson(json);
-      vm.messages = data;
+      // var data = angular.fromJson(json);
+      // const url = 'http://localhost:8082/api/messages';
+      const url = 'https://secret-bastion-10166.herokuapp.com/api/messages';
+      $http.get(url).then(function(messages) {
+        vm.messages = messages.data._embedded.messages
+        // console.log(messages.data._embedded.messages)
+      });
     }
   }
 }());
--- app/toolBar/toolbar_controller.js
+++ app/toolBar/toolbar_controller.js
@@ -4,90 +4,111 @@
     .module('app', )
     .controller('toolbarController', toolbarController)
 
-  function toolbarController() {
+  function toolbarController($http) {
     const vm = this;
     vm.$onInit = function() {
+
       vm.allSelected = function(messages) {
-        return status = messages.every(function(data) {
-          return data.selected == true;
-        })
+        if (messages !== undefined) {
+          return status = messages.every(function(data) {
+            return data.selected == true;
+          })
+        }
       }
       vm.someSelected = function(messages) {
-        const someMsgs = messages.some(function(data) {
-          return data.selected == true;
-        })
-        const allMsgs = messages.every(function(data) {
-          return data.selected == true;
-        })
-        return someMsgs && !allMsgs;
+        if (messages !== undefined) {
+          const someMsgs = messages.some(function(data) {
+            return data.selected == true;
+          })
+          const allMsgs = messages.every(function(data) {
+            return data.selected == true;
+          })
+          return someMsgs && !allMsgs;
+        }
       }
       vm.allNotSelected = function(messages) {
-        return status = messages.every(function(data) {
-          return data.selected !== true;
-        })
+        if (messages !== undefined) {
+          return status = messages.every(function(data) {
+            return data.selected !== true;
+          })
+        }
       }
     }
     vm.markAsRead = function(messages) {
-      for (var i = 0; i < messages.length; i++) {
-        if (messages[i].selected) {
-          messages[i].read = true;
-        }
-      }
-    }
-    vm.markAsUnRead = function(messages) {
-      for (var i = 0; i < messages.length; i++) {
-        if (messages[i].selected) {
-          messages[i].read = false;
-        }
-      }
-    }
-    vm.addLabel = function(messages, label) {
-      for (var i = 0; i < messages.length; i++) {
-        var labelExist = messages[i].labels.includes(label);
-        if (messages[i].selected && !labelExist) {
-          messages[i].labels.push(label);
-        }
-      }
-    }
-    vm.removeLabel = function(messages, label) {
-      for (var i = 0; i < messages.length; i++) {
-        if (messages[i].selected) {
-          var index = messages[i].labels.indexOf(label);
-          if (index > -1) {
-            messages[i].labels.splice(index, 1);
+      if (messages !== undefined) {
+        for (var i = 0; i < messages.length; i++) {
+          if (messages[i].selected) {
+            messages[i].read = true;
           }
         }
       }
     }
-    vm.changeSelect = function(data) {
-      if (vm.allSelected(data)) {
-        for (var i = 0; i < data.length; i++) {
-          data[i].selected = false;
+    vm.markAsUnRead = function(messages) {
+      if (messages !== undefined) {
+        for (var i = 0; i < messages.length; i++) {
+          if (messages[i].selected) {
+            messages[i].read = false;
+          }
         }
-      } else if (vm.someSelected(data)) {
-        for (var i = 0; i < data.length; i++) {
-          data[i].selected = true;
+      }
+    }
+    vm.addLabel = function(messages, label) {
+      if (messages !== undefined) {
+        for (var i = 0; i < messages.length; i++) {
+          var labelExist = messages[i].labels.includes(label);
+          if (messages[i].selected && !labelExist) {
+            messages[i].labels.push(label);
+          }
         }
-      } else if (vm.allNotSelected(data)) {
-        for (var i = 0; i < data.length; i++) {
-          data[i].selected = true;
+      }
+    }
+    vm.removeLabel = function(messages, label) {
+      if (messages !== undefined) {
+        for (var i = 0; i < messages.length; i++) {
+          if (messages[i].selected) {
+            var index = messages[i].labels.indexOf(label);
+            if (index > -1) {
+              messages[i].labels.splice(index, 1);
+            }
+          }
+        }
+      }
+    }
+    vm.changeSelect = function(messages) {
+      if (messages !== undefined) {
+        if (vm.allSelected(messages)) {
+          for (var i = 0; i < messages.length; i++) {
+            messages[i].selected = false;
+          }
+        } else if (vm.someSelected(messages)) {
+          for (var i = 0; i < messages.length; i++) {
+            messages[i].selected = true;
+          }
+        } else if (vm.allNotSelected(messages)) {
+          for (var i = 0; i < messages.length; i++) {
+            messages[i].selected = true;
+          }
         }
       }
     }
     vm.countUnreadMessages = function(messages) {
-      var count = 0;
-      for (var i = 0; i < messages.length; i++) {
-        if (messages[i].read == false) {
-          count++;
+      if (messages !== undefined) {
+        var count = 0;
+        for (var i = 0; i < messages.length; i++) {
+          if (messages[i].read == false) {
+            count++;
+          }
         }
+        return count;
       }
-      return count;
     }
     vm.deleteMessage = function(messages) {
-      for (var i = 0; i < messages.length; i++) {
-        if (messages[i].selected) {
-          messages.splice(i, 1);
-          i--;
+      if (messages !== undefined) {
+        for (var i = 0; i < messages.length; i++) {
+          if (messages[i].selected) {
+            messages.splice(i, 1);
+            i--;
+          }
         }
       }
     }
